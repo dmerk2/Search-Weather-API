9ecc51db3496c9ee6409061e81270f3abdb4c4e1
Daniel Merkin <dan.merkin@gmail.com>
2022-07-27 17:28:07 -0400

    fetch request from API

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,1 +1,21 @@
-console.log("connected");
+let response = document.getElementById('response');
+let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=trenton&appid=';
+
+function getApi(requestUrl) {
+  fetch(requestUrl)
+    .then(function (response) {
+      console.log(response.status);
+      //  Conditional for the the response.status.
+      if (response.status !== 200) {
+        // Place the response.status on the page.
+        responseText.textContent = response.status;
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      // Make sure to look at the response in the console and read how 404 response is structured.
+      console.log(data);
+    });
+}
+
+getApi(requestUrl);
